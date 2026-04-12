482f25711e38fef611678df3e50e0d101d36cedc
Pinhas <pinhasbent@gmail.com>
2026-04-12 12:03:01 +0200

    début Capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ventura.app',
+  appName: 'Ventura',
+  webDir: 'dist'
+};
+
+export default config;
