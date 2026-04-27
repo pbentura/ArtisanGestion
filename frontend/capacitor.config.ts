770ba0154f4232f2e95a3986a1da041145bbdce1
Pinhas <pinhasbent@gmail.com>
2026-04-27 12:10:35 +0200

    correction capacitor

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ventura.app',
+  appId: 'com.pinhasbentura.ventura',
   appName: 'Ventura',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
